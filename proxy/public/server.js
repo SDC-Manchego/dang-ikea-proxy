bf82d3fd558b56686bca734b2dc942e0f1e6c096
“silkyh13” <silkyh13@gmail.com>
2019-11-13 19:48:34 -0500

    deployed proxy

--- proxy/public/server.js
+++ proxy/public/server.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(morgan("dev"));
-
+app.use(morgan('dev'));
+app.use(proxy('/api-product-data', {target: 'http://localhost:3003'}));
+app.use(proxy('/api-reviews', {target: 'http://localhost:3003'}));
 app.use(express.static('public/lib'));
 app.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
