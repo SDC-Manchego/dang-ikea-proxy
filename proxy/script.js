ae4b5af2df0c05246d75df8b9d353f1b255fcfb2
“silkyh13” <silkyh13@gmail.com>
2019-11-15 02:33:31 -0500

    Added new relic

--- proxy/script.js
+++ proxy/script.js
@@ -9,20 +9,9 @@ export const options = {
 };
 
 export default function () {
-  const res1 = http.get(`http://ec2-3-15-12-216.us-east-2.compute.amazonaws.com:3003/?${Math.round(Math.random() * 1000000)}`);
 
-  const res2 = http.get(`http://ec2-3-15-12-216.us-east-2.compute.amazonaws.com:3003/${Math.round(Math.random() * 10000000)}`);
-
-  const res3 = http.get(`http://ec2-3-15-12-216.us-east-2.compute.amazonaws.com:3003/?id=${Math.round(Math.random() * 1000000)}`);
+  const res1 = http.get(`http://ec2-3-135-19-82.us-east-2.compute.amazonaws.com:3000/?${Math.round(Math.random()*1000000)}`);
   check(res1, {
-    'status was 200': (r) => r.status == 200,
-    'transaction time OK': (r) => r.timings.duration < 200,
-  });
-  check(res2, {
-    'status was 200': (r) => r.status == 200,
-    'transaction time OK': (r) => r.timings.duration < 200,
-  });
-  check(res3, {
     'status was 200': (r) => r.status == 200,
     'transaction time OK': (r) => r.timings.duration < 200,
   });
