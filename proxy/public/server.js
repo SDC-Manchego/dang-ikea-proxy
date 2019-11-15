b699ca8daaf3830eefd8386ea6f78337fbc642e0
“silkyh13” <silkyh13@gmail.com>
2019-11-15 01:34:56 -0500

    changed localhost to aws

--- proxy/public/server.js
+++ proxy/public/server.js
@@ -7,8 +7,8 @@ const port = 3000;
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(proxy('/api-product-data', {target: 'http://localhost:3003'}));
-app.use(proxy('/api-reviews', {target: 'http://localhost:3003'}));
+app.use(proxy('/api-product-data', {target: 'http://ec2-3-15-12-216.us-east-2.compute.amazonaws.com:3003/'}));
+app.use(proxy('/api-reviews', {target: 'http://ec2-3-15-12-216.us-east-2.compute.amazonaws.com:3003/'}));
 app.use(express.static('public/lib'));
 app.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
